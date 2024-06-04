5377c2c18027fb1511c4b37c7faa84417ea2b4ea
Dima Barovskiy <barovskiy@me.com>
2024-06-04 15:29:13 +0300

    fix_method_generate_random_string

--- helpers.py
+++ helpers.py
@@ -3,24 +3,21 @@ import requests
 import random
 import string
 
-# метод регистрации нового курьера возвращает список из логина и пароля
-# если регистрация не удалась, возвращает пустой список
+
+@allure.step('Генерируем строку, состоящую только из букв нижнего регистра, в качестве параметра передаём длину строки')
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    random_string = ''.join(random.choice(letters) for i in range(length))
+    return random_string
+
 @allure.step('Регистрируем курьера, возвращаем логин, пароль, имя курьера')
 def register_new_courier_and_return_login_password():
-    # метод генерирует строку, состоящую только из букв нижнего регистра, в качестве параметра передаём длину строки
-    def generate_random_string(length):
-        letters = string.ascii_lowercase
-        random_string = ''.join(random.choice(letters) for i in range(length))
-        return random_string
-
     # создаём список, чтобы метод мог его вернуть
     login_pass = []
-
     # генерируем логин, пароль и имя курьера
     login = generate_random_string(10)
     password = generate_random_string(10)
     first_name = generate_random_string(10)
-
     # собираем тело запроса
     payload = {
         "login": login,
@@ -30,7 +27,6 @@ def register_new_courier_and_return_login_password():
 
     # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
     response = requests.post('https://qa-scooter.praktikum-services.ru/api/v1/courier', data=payload)
-
     # если регистрация прошла успешно (код ответа 201), добавляем в список логин и пароль курьера
     if response.status_code == 201:
         login_pass.append(login)
@@ -58,11 +54,6 @@ def delete_courier(courier_id):
 
 @allure.step('Генерирум создание логина, пароля и имени курьера')
 def generate_login_password_first_name():
-    # метод генерирует строку, состоящую только из букв нижнего регистра, в качестве параметра передаём длину строки
-    def generate_random_string(length):
-        letters = string.ascii_lowercase
-        random_string = ''.join(random.choice(letters) for i in range(length))
-        return random_string
     # генерируем логин, пароль и имя курьера
     login = generate_random_string(10)
     password = generate_random_string(10)
@@ -78,10 +69,6 @@ def generate_login_password_first_name():
 
 @allure.step('Генерируем пароль и имя курьера')
 def generate_password_first_name():
-    def generate_random_string(length):
-        letters = string.ascii_lowercase
-        random_string = ''.join(random.choice(letters) for i in range(length))
-        return random_string
     password = generate_random_string(10)
     first_name = generate_random_string(10)
     payload = {
